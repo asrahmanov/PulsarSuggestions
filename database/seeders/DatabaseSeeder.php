b927c7a0eb57a15f6aa9c5985a357f2b3a8bb48f
asrahmanov <asrahmanvo@gmail.com>
2022-12-02 20:04:32 +0300

    Suggestions Status

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 
 use App\Models\Defect;
+use App\Models\SuggestionsStatus;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,6 +20,7 @@ class DatabaseSeeder extends Seeder
            SuggestionsCategorySeeder::class,
            SuggestionsSubdivisionSeeder::class,
            SuggestionsFAQSeeder::class,
+           SuggestionsStatusSeeder::class,
         ]);
     }
 }
